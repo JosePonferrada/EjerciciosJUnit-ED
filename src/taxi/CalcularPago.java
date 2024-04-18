122f2f6e922cb320995122c2687c05116de2bbdd
JosePonferrada <joseponfe10@gmail.com>
2024-04-18 13:11:12 +0200

    feat(JUnit): added some classes

---
+++ src/taxi/CalcularPago.java
@@ -0,0 +1,38 @@
+package taxi;
+
+import javax.swing.JOptionPane;
+
+public class CalcularPago {
+
+	public static void main(String[] args) {
+		
+		double kms;
+		
+		do {
+				
+		String kk = JOptionPane.showInputDialog("¿Cuántos kms ha recorrido?");
+		kms = Double.parseDouble(kk);
+		}
+		
+		while (kms < 0);
+	
+		System.out.println("El importe a pagar es: " + pago(kms) + "€");
+
+	}
+	
+	public static double pago(double kms) {
+		
+		double cantidad = 20;
+			
+		if (kms > 30) {
+			
+			if(kms < 100) {
+				cantidad += (kms - 30) * 0.6;
+			} else {
+				cantidad += ((70 * 0.6) + (kms - 100) * 0.4);
+			}
+		} 
+		return cantidad;
+	}
+
+}
---
+++ test/taxi/TaxiTest.java
@@ -0,0 +1,26 @@
+package taxi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TaxiTest {
+
+	@Test
+	void test0() {
+		double result = CalcularPago.pago(0);
+		double expected = 20;
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void test1() {
+		double result = CalcularPago.pago(19.9999999);
+		double expected = 19;
+		assertEquals(expected, result, 0.001); 
+		// Se puede introducir un margen de error como 3er parámetro
+	}
+	
+}
